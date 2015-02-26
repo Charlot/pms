77bf7e3f619a923a55a28fd7cca721799ea29fce
Charlot <7wangsong@gmail.com>
2015-02-26 17:35:06 +0800

    finish process entity create

--- pms-server/app/controllers/process_entities_controller.rb
+++ pms-server/app/controllers/process_entities_controller.rb
@@ -63,9 +63,24 @@ class ProcessEntitiesController < ApplicationController
                 end
               end
             elsif ProcessType.semi_auto?(@process_template.type)
+              params[:custom_field].each do |k, v|
+                puts "#{k}:#{v}"
 
+                if cf=CustomField.find_by_id(k)
+                  cv=CustomValue.new(custom_field_id: k, is_for_out_stock: params[:is_for_out_stock].has_key?(k), value: cf.get_field_format_value(v))
+                  @process_entity.custom_values<<cv
+                end
+              end
+
+              @process_entity.custom_values.each do |cv|
+                cf=cv.custom_field
+                if CustomFieldFormatType.part?(cf.field_format) #&& cf.is_for_out_stock
+                  puts "*************#{ params[:out_stock_qty][cf.id.to_s.to_sym]}"
+                  @process_entity.process_parts<<ProcessPart.new(part_id: cv.value, quantity: params[:out_stock_qty][cf.id.to_s.to_sym])
+                end
+              end
             end
-            raise
+            # raise
           end
 
           format.html { redirect_to @process_entity, notice: 'Process entity was successfully created.' }
