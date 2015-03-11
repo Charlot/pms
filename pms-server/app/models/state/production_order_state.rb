a0879580f628d34c37b6ccdcec23ed71a1c51022
Charlot <7wangsong@gmail.com>
2015-03-11 12:00:45 +0800

    add production order state

--- pms-server/app/models/state/production_order_state.rb
+++ pms-server/app/models/state/production_order_state.rb
@@ -1,12 +1,18 @@
 class ProductionOrderState
   INIT = 0
+  STARTED=1
+  FINISHED=2
 
   def self.display(state)
     case state
-    when INIT
-      'Init'
-    else
-      'Init'
+      when INIT
+        'Init'
+      when STARTED
+        '进行中'
+      when FINISHED
+        '已结束'
+      else
+        'Init'
     end
   end
 end
