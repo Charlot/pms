58351849e5d7efd4ebfba8aec2ba0c8cdbfe7e12
Charlot <7wangsong@gmail.com>
2015-03-19 16:41:02 +0800

    on watch

---
+++ pms-printer/PmsNCR/ClientDataWatcher.xaml.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using PmsNCRWcf.Config;
+using System.Timers;
+using System.IO;
+using Brilliantech.Framwork.Utils.LogUtil;
+
+namespace PmsNCR
+{
+    /// <summary>
+    /// ClientDataWatcher.xaml 的交互逻辑
+    /// </summary>
+    public partial class ClientDataWatcher : Window
+    {
+
+        private System.Timers.Timer scanTimer;
+
+        private static Object fileLocker = new Object();
+        private static Object dirLocker = new Object();
+         
+
+        public ClientDataWatcher()
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Minimized;
+        }
+
+
+
+        /// <summary>
+        /// init timer
+        /// </summary>
+        private void InitTimer()
+        {
+            this.scanTimer = new System.Timers.Timer();
+            ((System.ComponentModel.ISupportInitialize)(this.scanTimer)).BeginInit();
+            this.scanTimer.Enabled = true;
+            this.scanTimer.Interval = WPCSConfig.ScanClientFolderInterval;
+            this.scanTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.scanTimer_Elapsed);
+            ((System.ComponentModel.ISupportInitialize)(this.scanTimer)).EndInit();
+        }
+
+
+        /// <summary>
+        /// Scan File Timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void scanTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            scanTimer.Stop();
+            List<string> files = GetAllFilesFromDirectory(WPCSConfig.ClientDataDir);
+            foreach (string file in files)
+            {
+                if (IsFileClosed(file))
+                {
+                    Process(file);
+                }
+            }
+            scanTimer.Enabled = true;
+            scanTimer.Start();
+        }
+
+        /// <summary>
+        /// Process File
+        /// </summary>
+        /// <param name="fullPath"></param>
+        private void Process(string fullPath)
+        {
+            bool canRemoveErrorFile = true;
+
+            string nextDir = System.IO.Path.Combine(WPCSConfig.ScanedFileClientFolder, DateTime.Today.ToString("yyyy-MM-dd"));
+            try
+            {
+                if (IsFileClosed(fullPath))
+                {
+                    using (FileStream fs = File.Open(fullPath, FileMode.Open, FileAccess.Read))
+                    {
+                        using (StreamReader reader = new StreamReader(fs))
+                        {
+                             
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                LogUtil.Logger.Error(e.GetType()); 
+                canRemoveErrorFile = false;
+                LogUtil.Logger.Error(e.Message);
+            }
+            // 是否可以访问服务 不可以访问时保持文件不处理
+            if (canRemoveErrorFile)
+            {
+                // 是否删除文件
+                if (WPCSConfig.DeleteFileAfterRead)
+                {
+                    // 删除文件
+                    if (IsFileClosed(fullPath))
+                    {
+                        File.Delete(fullPath);
+                        LogUtil.Logger.Warn("【删除读完的数据文件】" + fullPath);
+                    }
+                }
+                else
+                {
+                    // 移动文件
+                    CheckDirectory(nextDir);
+                    MoveFile(fullPath, System.IO.Path.Combine(nextDir, System.IO.Path.GetFileName(fullPath)));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get all files in directory
+        /// </summary>
+        /// <param name="direcctory"></param>
+        /// <returns></returns>
+        private List<string> GetAllFilesFromDirectory(string directory)
+        {
+            try
+            {
+                return Directory.GetFiles(directory.Trim()).ToList();
+            }
+            catch (Exception e)
+            {
+                LogUtil.Logger.Error("【获取所有文件出现异常】" + e.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Move file
+        /// </summary>
+        /// <param name="sourceFileName"></param>
+        /// <param name="destFileName"></param>
+        /// <param name="autoRename"></param>
+        private static void MoveFile(string sourceFileName, string destFileName, bool autoRename = true)
+        {
+            try
+            {
+                lock (fileLocker)
+                {
+                    if (File.Exists(sourceFileName))
+                    {
+                        if (autoRename)
+                        {
+                            destFileName = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(destFileName),
+                                DateTime.Now.ToString("HHmmSSS") + "_" 
+                                +System.IO.Path.GetFileNameWithoutExtension(sourceFileName) 
+                                + "_" + Guid.NewGuid().ToString() + System.IO.Path.GetExtension(sourceFileName));
+                        }
+                        else
+                        {
+                            throw new IOException("目标文件已经存在");
+                        }
+                    }
+                    File.Move(sourceFileName, destFileName);
+                    LogUtil.Logger.Info("【文件移动】【自】" + sourceFileName + "【至】" + destFileName);
+                }
+            }
+            catch (Exception e)
+            {
+                LogUtil.Logger.Error("【文件移动】【自】" + sourceFileName + "【至】" + destFileName + "【错误】" + e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Check directory
+        /// </summary>
+        /// <param name="dirName"></param>
+        /// <param name="autoCreate"></param>
+        /// <returns></returns>
+        private static bool CheckDirectory(string dirName, bool autoCreate = true)
+        {
+            bool result = false;
+            lock (dirLocker)
+            {
+                if (Directory.Exists(dirName))
+                {
+                    result = true;
+                }
+                else
+                {
+                    if (autoCreate)
+                    {
+                        Directory.CreateDirectory(dirName);
+                        result = true;
+                    }
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Check file is closed 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        private bool IsFileClosed(string fileName)
+        {
+            try
+            {
+                using (File.Open(fileName, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+                {
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                LogUtil.Logger.Warn(fileName + "文件未关闭." + e.Message);
+                return false;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.WindowState = WindowState.Minimized;
+        }
+
+
+    }
+}
--- pms-printer/PmsNCRWcf/Config/WPCSConfig.cs
+++ pms-printer/PmsNCRWcf/Config/WPCSConfig.cs
@@ -3,49 +3,141 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using Brilliantech.Framwork.Utils.ConfigUtil;
 
 namespace PmsNCRWcf.Config
 {
     public class WPCSConfig
     {
-        private static string orderFolder;
-        private static string orderDir;
+        private static ConfigUtil config;
 
-        private static string dataFolder;
-        private static string dataDir;
-        
+        private static string serverOrderFolder;
+        private static string serverOrderDir;
+
+        private static string serverDataFolder;
+        private static string serverDataDir;
+
+        private static string clientDataFolder;
+        private static string clientDataDir;    
+
+        private static string machineNr;
+        private static string machineIP;
+
+        private static int scanClientFolderInterval = 1000;
+        private static bool deleteFileAfterRead = false;
+
+        private static string scanedFileClientFolder;    
+        private static string scanedFileClientDir;
 
         static WPCSConfig()
         {
-            orderFolder = "Order";
-            orderDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, orderFolder);
+            config = new ConfigUtil("WPCS", "Ini/wpcs.ini");
 
-            dataFolder = "WPCSData";
-            dataDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, dataFolder);
+            serverOrderFolder = config.Get("ServerOrderFolder");
+            serverOrderDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, serverOrderFolder);
+
+            serverDataFolder = config.Get("ServerDataFolder");
+            serverDataDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, serverDataFolder);
+
+            clientDataFolder = config.Get("ClientDataFolder");
+            clientDataDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, clientDataFolder);
+
+            machineNr = config.Get("MachineNr");
+            machineIP = config.Get("MachineIP");
+
+            if (!int.TryParse(config.Get("ScanClientFolderInterval"), out scanClientFolderInterval))
+            {
+                scanClientFolderInterval = 1000;
+            }
+
+            if (!bool.TryParse(config.Get("DeleteFileAfterRead"), out deleteFileAfterRead))
+            {
+                deleteFileAfterRead = false;
+            }
+
+            scanedFileClientFolder = config.Get("ScanedFileClientFolder");
+            scanedFileClientDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, scanedFileClientFolder);
         }
 
-        public static string OrderFolder
+
+        /// <summary>
+        /// get server order full path by order file name
+        /// </summary>
+        /// <param name="fileName">file name</param>
+        /// <returns></returns>
+        public static string GetServerOrderFullPath(string fileName)
         {
-            get { return WPCSConfig.orderFolder; }
-            set { WPCSConfig.orderFolder = value; }
+            return Path.Combine(WPCSConfig.ServerOrderDir, fileName);
         }
-        public static string DataFolder
+
+        public static string ServerOrderFolder
         {
-            get { return WPCSConfig.dataFolder; }
-            set { WPCSConfig.dataFolder = value; }
+            get { return WPCSConfig.serverOrderFolder; }
+            set { WPCSConfig.serverOrderFolder = value; }
         }
-
-        public static string OrderDir {
-            get { return WPCSConfig.orderDir; }
-        }
-
-        public static string DataDir
+        public static string ServerDataFolder
         {
-            get { return WPCSConfig.dataDir; }
+            get { return WPCSConfig.serverDataFolder; }
+            set { WPCSConfig.serverDataFolder = value; }
         }
 
-        public static string GetFullPath(string fileName) {
-            return Path.Combine(WPCSConfig.OrderDir, fileName);
+        public static string ServerOrderDir
+        {
+            get { return WPCSConfig.serverOrderDir; }
         }
+
+        public static string ServerDataDir
+        {
+            get { return WPCSConfig.serverDataDir; }
+        }
+
+        public static string ClientDataFolder
+        {
+            get { return WPCSConfig.clientDataFolder; }
+            set { WPCSConfig.clientDataFolder = value; }
+        }
+
+        public static string ClientDataDir
+        {
+            get { return WPCSConfig.clientDataDir; }
+            set { WPCSConfig.clientDataDir = value; }
+        }
+
+
+        public static string MachineNr
+        {
+            get { return WPCSConfig.machineNr; }
+            set { WPCSConfig.machineNr = value; }
+        }
+        public static string MachineIP
+        {
+            get { return WPCSConfig.machineIP; }
+            set { WPCSConfig.machineIP = value; }
+        }
+
+        public static int ScanClientFolderInterval
+        {
+            get { return WPCSConfig.scanClientFolderInterval; }
+            set { WPCSConfig.scanClientFolderInterval = value; }
+        }
+
+        public static bool DeleteFileAfterRead
+        {
+            get { return WPCSConfig.deleteFileAfterRead; }
+            set { WPCSConfig.deleteFileAfterRead = value; }
+        }
+
+        public static string ScanedFileClientFolder
+        {
+            get { return WPCSConfig.scanedFileClientFolder; }
+            set { WPCSConfig.scanedFileClientFolder = value; }
+        }
+        public static string ScanedFileClientDir
+        {
+            get { return WPCSConfig.scanedFileClientDir; }
+            set { WPCSConfig.scanedFileClientDir = value; }
+        }
+
+
     }
 }
