34e256f524faaaad978329060219ec7050ab9685
Charlot <7wangsong@gmail.com>
2015-03-29 16:41:27 +0800

    update

--- pms-server/app/models/process_template.rb
+++ pms-server/app/models/process_template.rb
@@ -6,7 +6,7 @@ class ProcessTemplate < ActiveRecord::Base
   validates :code, presence: {message: 'code cannot be blank'}, uniqueness: {message: 'code should be uniq'}
   validates :template, presence: {message: 'template cannot be blank'}, if: Proc.new { |p| ProcessType.semi_auto?(p.type) }
   has_many :custom_fields, lambda { order("#{CustomField.table_name}.position") }, as: :custom_fieldable
-
+  has_many :process_entities, dependent: :destroy
   before_create :parse_template_into_cf, if: Proc.new { |p| ProcessType.semi_auto?(p.type) }
   after_create :parse_cf_into_template, if: Proc.new { |p| ProcessType.semi_auto?(p.type) }
   acts_as_customizable
@@ -32,7 +32,7 @@ class ProcessTemplate < ActiveRecord::Base
       index[format_key] +=1
       count=i
     end
-    self.custom_fields << CustomField.build_by_format("part","default_wire_nr",self.custom_field_type,count+1)
+    self.custom_fields << CustomField.build_by_format("part", "default_wire_nr", self.custom_field_type, count+1)
     puts '*************************'
     puts self.template
     puts '*************************'
@@ -58,52 +58,56 @@ class ProcessTemplate < ActiveRecord::Base
   def template_custom_field_ids
     @template_custom_field_ids||=self.template.scan(/{(\d+)}/).map(&:first)
   end
-  
+
   # 导入自动模版
-  
-  def self.import(file,import_type)
+
+  def self.import(file, import_type)
     spreadsheet = open_spreadsheet(file)
     header = spreadsheet.row(1)
     (2..spreadsheet.last_row).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
-      allowed_attributes = [ "code","name","template","description"]
+      allowed_attributes = ["code", "name", "template", "description"]
       process_template = find_by_code(row["code"]) || new
-      process_template.attributes = row.to_hash.select { |k,v| allowed_attributes.include? k}
-      
+      process_template.attributes = row.to_hash.select { |k, v| allowed_attributes.include? k }
+
       ProcessTemplate.transaction do
-        
+
         case import_type
           when 'auto'
             process_template.type = 100
-            auto_attributes = ["default_wire_nr","wire_nr","wire_qty_factor","default_bundle_qty","t1","t1_default_strip_length","t1_qty_factor","t1_strip_length","t2","t2_qty_factor","t2_default_strip_length","t2_strip_length","s1","s1_qty_factor","s2","s2_qty_factor"]
+            auto_attributes = ["default_wire_nr", "wire_nr", "wire_qty_factor", "default_bundle_qty", "t1", "t1_default_strip_length", "t1_qty_factor", "t1_strip_length", "t2", "t2_qty_factor", "t2_default_strip_length", "t2_strip_length", "s1", "s1_qty_factor", "s2", "s2_qty_factor"]
             custom_fields = {}
-            custom_fields = row.to_hash.select { |k1,v1| auto_attributes.include? k1 }
-        
+            custom_fields = row.to_hash.select { |k1, v1| auto_attributes.include? k1 }
+
             ProcessTemplateAuto.build_custom_fields(custom_fields.select { |k, v| !v.nil? }.keys, process_template).each do |cf|
               # cf.save
               process_template.custom_fields<<cf
             end
             process_template.save!
-            
+
           when 'semi'
-            
+
           when 'manual'
             process_template.type = 300
             process_template.save!
           else
             raise ' No such process template'
         end
-      
+
       end
     end
   end
-  
+
   def self.open_spreadsheet(file)
-      case File.extname(file.original_filename)
-      when ".csv" then Roo::Csv.new(file.path)
-      when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
-      when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
-      else raise "Unknown file type: #{file.original_filename}"
-      end
+    case File.extname(file.original_filename)
+      when ".csv" then
+        Roo::Csv.new(file.path)
+      when ".xls" then
+        Roo::Excel.new(file.path, nil, :ignore)
+      when ".xlsx" then
+        Roo::Excelx.new(file.path, nil, :ignore)
+      else
+        raise "Unknown file type: #{file.original_filename}"
+    end
   end
 end
