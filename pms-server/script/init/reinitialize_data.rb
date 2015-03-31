a229183478f0427fa979e1531a2ed87a75d7345a
Tesla <leechee89@gmail.com>
2015-03-31 13:54:39 +0800

    fix

--- pms-server/script/init/reinitialize_data.rb
+++ pms-server/script/init/reinitialize_data.rb
@@ -1,7 +1,7 @@
 Kanban.where(ktype:KanbanType::WHITE).each do |k|
   if k.process_entities.count > 1
     if k.process_entities.where(product_id:k.product_id).count <=0
-      puts "需要重新建立关联".yellow
+      puts "需要重新建立关联 #{k.nr}".yellow
       pe_nr = k.process_entities.first.nr
       process_entity = ProcessEntity.where(nr:pe_nr,product_id:k.product_id).first
       if process_entity
@@ -9,13 +9,13 @@ Kanban.where(ktype:KanbanType::WHITE).each do |k|
           new_pe = KanbanProcessEntity.new(kanban_id:k.id,process_entity_id:process_entity.id)
           k.process_entities << new_pe
           k.save
-          k.process_entities.where(product_id:k.product_id).each{|pe|pe.destroy}
+          k.kanban_process_entities.where("process_entity_id not ?",process_entity.id).each{|pe|pe.destroy}
         end
       else
-        puts "未找到Routing，步骤号#{pe_nr}，总成号#{k.product_nr}"
+        puts "未找到Routing，步骤号#{pe_nr}，总成号#{k.product_nr}".red
       end
     else
-      k.process_entities.where(product_id:k.product_id).each{|pe|pe.destroy}
+      k.kanban_process_entities.joins(:process_entity).where("process_entities.product_id != ?",k.product_id).each{|e|e.destroy}
     end
   else
     puts "不需要修改".green
