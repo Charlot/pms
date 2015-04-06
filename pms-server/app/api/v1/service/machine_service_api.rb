66be836feba4f237b0e5c13b025ca2b2cc31eb77
Charlot <7wangsong@gmail.com>
2015-04-07 05:22:05 +0800

    set machine ip

--- pms-server/app/api/v1/service/machine_service_api.rb
+++ pms-server/app/api/v1/service/machine_service_api.rb
@@ -1,24 +1,29 @@
 module V1
   module Service
     class MachineServiceAPI<ServiceBase
-      namespace :machine do
-        guard_all!
-        namespace :setting do
-          post :ip do
-            msg=Message.new
-            if machine=Machine.find_by_nr(params[:machine])
-              if params[:ip]=~Resolv::IPv4::Regex
-                msg.result = machine.update(ip: params[:ip])
-              else
-                msg.content = "Machine IP #{params[:ip]} is not valid"
-              end
-            else
-              msg.content="No Machine Nr: #{params[:machine]}"
-            end
-            msg
-          end
-        end
-      end
+
+      
+      # namespace :machine do
+      #   guard_all!
+      #   namespace :setting do
+      #     post :ip do
+      #       msg=ServiceMessage.new
+      #       if machine=Machine.find_by_nr(params[:machine_nr])
+      #         if params[:ip]=~Resolv::IPv4::Regex
+      #           msg.Result = machine.update(ip: params[:ip])
+      #         else
+      #           msg.Content = "Machine IP #{params[:ip]} is not valid"
+      #         end
+      #       else
+      #         msg.Content="No Machine Nr: #{params[:machine_nr]}"
+      #       end
+      #       msg
+      #     end
+      #   end
+      # end
+      #
+
+
     end
   end
 end
