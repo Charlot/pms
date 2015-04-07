49c4e2078b4d1da7217dc59bcf21d2b857fab17b
Tesla <leechee89@gmail.com>
2015-04-07 18:03:38 +0800

    chmod

--- pms-server/script/tool/remove_routing_by_type.rb
+++ pms-server/script/tool/remove_routing_by_type.rb
@@ -1,17 +1,18 @@
 if ProcessType.include_value?(ARGV[0])
   process_entities = ProcessEntity.joins(:process_template).where("process_templates.type = ?",ARGV[0])
-  puts process_entities.to_json
+  #puts process_entities.to_json
   if process_entities.count == 0
     puts "================"
     puts "Routing未找到"
     puts "================"
   else
+    count = process_entities.count
     ProcessEntity.transaction do
       begin
         process_entities.each{|pe|
           pe.destroy
         }
-        puts "删除#{process_entities.count}个Routing!"
+        puts "删除#{count}个Routing!"
       rescue => e
         puts e.message
       end
