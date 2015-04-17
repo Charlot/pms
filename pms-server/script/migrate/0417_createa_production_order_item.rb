e752bfca39b089328d0693deb8cfac7540c80305
Charlot <7wangsong@gmail.com>
2015-04-17 14:53:22 +0800

    fix

--- pms-server/script/migrate/0417_createa_production_order_item.rb
+++ pms-server/script/migrate/0417_createa_production_order_item.rb
@@ -1,17 +1,10 @@
-=begin
-product = Part.where({nr:"93NMA001A",type:PartType::PRODUCT}).first
+puts "1111"
+product = Part.where({nr:"93S351313",type:PartType::PRODUCT}).first
 nrs = [
-    'LB50','MB10','MA35','MA41',
-    'MB50','MA20','MB40','PI24',
-    'ZK00','EP01','ZEP1','DE21',
-    'ZL04','ZE50','ZE20','ZE21',
-    'ZH00','ZZ90','ZA05','LD30(1)',
-    'QA50','PI51(1)','LB35','LB34',
-    'AB10','DA07-','DA07(1)','ST01',
-    'ZP86','ZL01','MC34','PF20',
-    'AB10','LB35','HL04(1)','AA38',
-    'LD20','HC45','HC45-','HA61-'
-    ]
+    'AB31-1','BB36-1','MC10-1','MC30-1',
+    'EAL1-1','EAL2-1','AB31-1A','BB38-1',
+    'MC31-1','MC11-1','BB38-1A'
+]
 pes = ProcessEntity.where(product_id:product.id).select{|pe| nrs.include? pe.parsed_wire_nr}
 i = 1
 pes.each{|pe|
@@ -61,4 +54,4 @@ pes.each{|pe|
       puts "步骤不存在！或步骤不消耗零件!".red
     end
   end
-}=end
+}
