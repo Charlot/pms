990198ce741e555b2d80ea5636103b2066f05aaf
Charlot <7wangsong@gmail.com>
2015-04-18 11:30:43 +0800

    script

--- pms-server/script/migrate/0417_createa_production_order_item.rb
+++ pms-server/script/migrate/0417_createa_production_order_item.rb
@@ -1,9 +1,13 @@
 puts "1111"
-product = Part.where({nr:"93S351313",type:PartType::PRODUCT}).first
+product = Part.where({nr:"93NMA001A",type:PartType::PRODUCT}).first
 nrs = [
-    'AB31-1','BB36-1','MC10-1','MC30-1',
-    'EAL1-1','EAL2-1','AB31-1A','BB38-1',
-    'MC31-1','MC11-1','BB38-1A'
+	'ZE20','ZE21','ZZ91','ZJ08',
+	'ZH00','DE21','MA20','LB35',
+	'EB21','LB36','HC42','ZL01',
+	'DA07','DA07(1)','LB34','LB35',
+	'PI51(1)','MC34','PI48(1)','PI48-',
+	'PI54','HL04(1)','LB36','HA61-',
+	'HC45','AA38','LD20','ZP86'
 ]
 pes = ProcessEntity.where(product_id:product.id).select{|pe| nrs.include? pe.parsed_wire_nr}
 i = 1
@@ -12,6 +16,8 @@ pes.each{|pe|
   if kanban
 
     @kanban = kanban
+
+    puts "#{@kanban.wire_nr},#{@kanban.quantity}"
 
     if @kanban.quantity <= 0
       next
@@ -27,6 +33,7 @@ pes.each{|pe|
 
     process_entity = @kanban.process_entities.first
     if process_entity && process_entity.process_parts.count > 0
+=begin
       can_create = true
       parts = []
       process_entity.process_parts.each { |pe|
@@ -48,8 +55,9 @@ pes.each{|pe|
           next
         end
 
-        puts "新建订单成功：#{@kanban.nr},#{parts.join('-')}".green
+        puts "新建订单成功：#{@kanban.nr},#{@kanban.wire_nr}".green
       end
+=end
     else
       puts "步骤不存在！或步骤不消耗零件!".red
     end
