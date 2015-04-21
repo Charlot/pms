87cf035d18022fc99df5996ab7f8a39f2fb10f27
Tesla <leechee89@gmail.com>
2015-04-22 03:40:54 +0800

    fix

--- pms-server/app/services/file_handler/csv/machine_combination_handler.rb
+++ pms-server/app/services/file_handler/csv/machine_combination_handler.rb
@@ -49,6 +49,7 @@ module FileHandler
             MachineCombination.transaction do
               CSV.foreach(file.file_path, headers: file.headers, col_sep: file.col_sep, encoding: file.encoding) do |row|
                 row.strip
+                puts "#{row}".red
                 params = {}
                 machine = Machine.find_by_nr(row['Machine Nr'])
                 params[:machine_id] = machine.id
@@ -65,7 +66,7 @@ module FileHandler
 
                 if machine_combination
                   puts "更新".red
-                  machine_combination.update(params)
+                  #machine_combination.update(params)
                 else
                   puts "新建".red
                   MachineCombination.create(params)
@@ -80,6 +81,7 @@ module FileHandler
           end
         rescue => e
           puts e.backtrace
+          msg.result = false
           msg.content = e.message
         end
         return msg
