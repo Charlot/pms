d8ab7788676328adf3b10b38891bb1f07bffbf68
Charlot <7wangsong@gmail.com>
2015-04-21 16:14:54 +0800

    options

--- pms-server/app/helpers/production_orders_helper.rb
+++ pms-server/app/helpers/production_orders_helper.rb
@@ -1,5 +1,15 @@
 module ProductionOrdersHelper
   def production_order_options
-    ProductionOrder.limit(30).order(id: :desc).collect { |p| [p.nr, p.id] }
+    ProductionOrder.offset(offset).limit(4).collect { |p| [p.nr, p.id] }
+  end
+
+  # best in place options
+  def production_order_bip_options(production_order)
+    ProductionOrder.where.not(id: production_order.id).offset(offset).limit(30).collect { |p| [p.id, p.nr] }<<[production_order.id, production_order.nr]
+  end
+
+  private
+  def offset
+    ProductionOrder.count<30 ? 0 : (ProductionOrder.count-30)
   end
 end
