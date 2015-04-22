e9f6788f01ba1ebb2e7c1d4d3601aef2f2ee18b6
Charlot <7wangsong@gmail.com>
2015-04-22 13:39:43 +0800

    fix

--- pms-server/script/migrate/update_kanban_quantity.rb
+++ pms-server/script/migrate/update_kanban_quantity.rb
@@ -1,16 +1,7 @@
-Kanban.where(ktype:KanbanType::WHITE).each do |k|
-  if k.quantity>0 &&k.quantity < 200
-    if k.bundle <=0
-      #k.update(bundle:k.quantity)
-      #puts "<0".green
-      puts "=================="
-    else
-      if (k.quantity % k.bundle )> 0
-        puts ">0".red
-        k.update(bundle:k.quantity)
-      else
-        #puts "#{k.quantity % k.bundle}"
-      end
-    end
+prouct = Part.find_by_nr("93NMA001A");
+Kanban.where("ktype = #{KanbanType::WHITE} and product_id = #{prouct.id} and des_storage like '%XM%' ").each do |k|
+  puts "#{k.quantity},#{k.des_storage}"
+  if k.quantity > 0
+    k.update(quantity:50)
   end
 end
