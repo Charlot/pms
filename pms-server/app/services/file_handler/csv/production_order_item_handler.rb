ed596d1ba09fb3d860b23e48ccb0d7f581fb965b
Charlot <7wangsong@gmail.com>
2015-04-27 17:39:52 +0800

    update remark

--- pms-server/app/services/file_handler/csv/production_order_item_handler.rb
+++ pms-server/app/services/file_handler/csv/production_order_item_handler.rb
@@ -75,6 +75,18 @@ module FileHandler
         end
         msg
       end
+
+      def self.get_encoding(user_agent)
+        os=System::Base.os_by_user_agent(user_agent)
+        case os
+          when 'windows'
+            return 'GB18030:UTF-8'
+          when 'linux', 'macintosh'
+            return 'UTF-8:UTF-8'
+          else
+            return 'UTF-8:UTF-8'
+        end
+      end
     end
   end
 end
