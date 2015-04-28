3b5adcfa32017fe65a5e5c8c8cd0f89508615559
Charlot <7wangsong@gmail.com>
2015-04-28 14:35:37 +0800

    push

--- pms-server/app/services/file_handler/csv/production_order_item_handler.rb
+++ pms-server/app/services/file_handler/csv/production_order_item_handler.rb
@@ -5,6 +5,7 @@ module FileHandler
       # EXPORT_CSV_HEADERS=%w(No OrderItemNr Kanban Machine OrderNr PartNr Material)
       STATE_EXPORT_CSV_HEADERS=%w(No ItemNr OrderNr State OptimiseIndex Machine  KanbanNr ProductNr KanbanWireNr KanbanQuantity KanbanBundle ProducedQty WireNr Diameter WireLength Terminal1Nr Tool1Nr Terminal1StripLength Terminal2Nr Tool2Nr Terminal2StripLength Seal1Nr Seal2Nr UpdateTime)
       EXPORT_CSV_HEADERS=STATE_EXPORT_CSV_HEADERS
+
       def export_optimized(items, user_agent, tmp_path=nil)
         msg=Message.new
         begin
@@ -24,7 +25,7 @@ module FileHandler
             #   ]
             # end
             items=ProductionOrderItemPresenter.init_preview_presenters(items)
-            items.each_with_index do |item,i|
+            items.each_with_index do |item, i|
               row=[]
               STATE_EXPORT_CSV_HEADERS.each do |head|
                 row<< (head=='State' ? ProductionOrderItemState.display(item[:State]) : item[head.to_sym])
@@ -60,11 +61,13 @@ module FileHandler
 
             items=ProductionOrderItemPresenter.init_preview_presenters(q.all)
             items.each do |item|
-              row=[]
-              STATE_EXPORT_CSV_HEADERS.each do |head|
-                row<< (head=='State' ? ProductionOrderItemState.display(item[:State]) : item[head.to_sym])
+              if item
+                row=[]
+                STATE_EXPORT_CSV_HEADERS.each do |head|
+                  row<< (head=='State' ? ProductionOrderItemState.display(item[:State]) : item[head.to_sym])
+                end
+                csv<<row
               end
-              csv<<row
             end
             csv<<[] if items.count==0
           end
