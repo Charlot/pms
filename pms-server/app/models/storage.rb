7fa09f5fce9fa4efacceb308bfbabeca09e7d554
Tesla <leechee89@gmail.com>
2015-04-29 22:40:21 +0800

    fix

--- pms-server/app/models/storage.rb
+++ pms-server/app/models/storage.rb
@@ -8,11 +8,20 @@ class Storage < ActiveRecord::Base
 
   validates_uniqueness_of :part_id, :scope => :position_id
 
-  def self.add(part,quantity,position)
-    part = Part.find_by_nr(part)
-    position = Position.find_by_detail(position)
-
+  def self.add(part_nr,quantity,position_detail=nil)
+    part = Part.find_by_nr(part_nr)
     raise "Part not found" if part.nil?
+
+    if position_detail.nil?
+      storage = where({part_id:part.id,position_id: nil}).first
+      if storage
+        storage.update(quantity:storage.quantity+quantity)
+      else
+        create({part_id:part.id,quantity:quantity})
+      end
+    end
+
+    position = Position.find_by_detail(position_detail)
     raise "Position not found" if position.nil?
 
     storage = where({part_id:part.id,position_id: position.id}).first
@@ -24,11 +33,24 @@ class Storage < ActiveRecord::Base
     end
   end
 
-  def self.reduce(part,quantity,position)
-    part = Part.find_by_nr(part)
-    position = Position.find_by_detail(position)
-
+  def self.reduce(part_nr,quantity,position_detail=nil)
+    part = Part.find_by_nr(part_nr)
     raise "Part not found" if part.nil?
+
+    if position_detail
+      storage = where({part_id:part.id,position_id: nil}).first
+      if storage
+        if storage.quantity >= quantity
+          storage.update(quantity:storage.quantity-quantity)
+        else
+          raise "Storage is out of stock"
+        end
+      else
+        raise "Storage not found"
+      end
+    end
+
+    position = Position.find_by_detail(position_detail)
     raise "Position not found" if position.nil?
 
     storage = where({part_id:part.id,position_id: position.id}).first
