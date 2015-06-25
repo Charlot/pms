90eb96bf3d42f58771f5dc7b0a9231854806b052
Charlot <7wangsong@gmail.com>
2015-06-25 11:17:30 +0800

    unit

--- pms-server/db/migrate/20150625025159_add_convert_unit_to_parts.rb
+++ pms-server/db/migrate/20150625025159_add_convert_unit_to_parts.rb
@@ -1,5 +1,5 @@
 class AddConvertUnitToParts < ActiveRecord::Migration
   def change
-    add_column :parts, :convert_unit, :decimal
+    add_column :parts, :convert_unit, :decimal,default:1
   end
 end
