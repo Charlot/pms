b1a83b5355e18593832514ef7105677e3487c9af
Charlot <7wangsong@gmail.com>
2015-06-26 11:19:09 +0800

    fix

--- pms-server/script/migrate/0626check_stock/04_fix_kanban_position.rb
+++ pms-server/script/migrate/0626check_stock/04_fix_kanban_position.rb
@@ -17,3 +17,17 @@ Kanban.transaction do
     end
   end
 end
+ProductionOrderItemLabel.transaction do
+  ProductionOrderItemLabel.where("position_nr like ' % '").each_with_indx do |l,i|
+    puts "#{i}---#{l.nr}".red
+
+    position_nr=Warehouse::DEFAULT_POSITION
+    whouse_nr=Warehouse::DEFAULT_WAREHOUSE
+    begin
+      position_nr=l.position_nr.strip
+      whouse_nr=Warehouse.get_whouse_by_position_prefix(position_nr)
+    end
+
+    l.update_attributes(position_nr: position_nr, whouse_nr: whouse_nr)
+  end
+end
