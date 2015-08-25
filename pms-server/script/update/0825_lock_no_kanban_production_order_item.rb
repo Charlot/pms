ba251acd63b4777916eb6c00b850503c767f5942
Charlot <7wangsong@gmail.com>
2015-08-25 12:18:05 +0800

    add puts

--- pms-server/script/update/0825_lock_no_kanban_production_order_item.rb
+++ pms-server/script/update/0825_lock_no_kanban_production_order_item.rb
@@ -3,6 +3,7 @@ ProductionOrderItem.for_produce.all.each do |item|
   puts item.to_json
   # puts kanbanz.red
   unless (kanban && kanban.process_entities.first)
+    puts item.to_json.yellow
     item.update(state:ProductionOrderItemState::SYSTEM_ABORTED,message:'被删除，无法生产，系统停止')
   end
 end
