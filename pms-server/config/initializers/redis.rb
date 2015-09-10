5aa3d63da200d05454c69e8fa536409ac48490dc
Charlot <7wangsong@gmail.com>
2015-09-10 13:44:47 +0800

    change to test

--- pms-server/config/initializers/redis.rb
+++ pms-server/config/initializers/redis.rb
@@ -2,13 +2,13 @@ require 'redis'
 require 'redis-namespace'
 
 
-$redis= Redis::Namespace.new('pms-server', redis: Redis.new(:host => '127.0.0.1', :port => '6379', :db => 0))
+$redis= Redis::Namespace.new('test-pms-server', redis: Redis.new(:host => '127.0.0.1', :port => '6379', :db => 3))
 
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
     if forked
       $redis.client.disconnect
-      $redis= Redis::Namespace.new('pms-server', redis: Redis.new(:host => '127.0.0.1', :port => '6379', :db => 0))
+      $redis= Redis::Namespace.new('test-pms-server', redis: Redis.new(:host => '127.0.0.1', :port => '6379', :db => 3))
     end
   end
 end
