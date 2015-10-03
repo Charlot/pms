c7e835cd0ade20c9d8675a1a9befb7a58810afac
Charlot <7wangsong@gmail.com>
2015-10-03 18:14:16 +0800

    add kanban update state change auto count

--- pms-server/app/models/concerns/kanban_auto_countable.rb
+++ pms-server/app/models/concerns/kanban_auto_countable.rb
@@ -6,34 +6,49 @@ module KanbanAutoCountable
 
   def recount_auto_copy
     if self.is_a?(Kanban)
-      if self.product_id_changed?
-        count_kanban({product_id: [self.product_id, self.product_id_was],
-                      process_entity_id: process_entity_id}) &update_rac_proc
-      end
+      #if self.product_id_changed?
+        #count_kanban({product_id: [self.product_id, self.product_id_was],
+                      #process_entity_id: process_entity_id},update_rac_proc)
+      #end
 
       if self.state_changed?
-        count_kanban({product_id: self.product_id, process_entity_id: process_entity_id}) &update_rac_proc
-      end
+		  options={product_id: self.product_id, process_entity_id: process_entity_id}
+      if base_query_kanban(options).count.first.nil? 
+		  update_recount_auto_copy(self.product_id,process_entity_id,0)
+	  else
+		  query_kanban(options,update_rac_proc)
+	  end
+	  end
     end
   end
 
   private
-  def count_kanban(options={})
-    base_query= Kanban.joins(:kanban_process_entities)
+
+  def base_query_kanban options={}
+    q= Kanban.joins(:kanban_process_entities)
                     .where(kanban_process_entities: {position: 0})
                     .where.not(kanbans: {state: KanbanState::DELETED})
                     .group(:product_id, :process_entity_id)
-                    .select('count(*) as size,product_id,process_entity_id')
+
 
     if options[:product_id]
-      base_query=base_query.where(product_id: options[:product_id])
+      q=q.where(product_id: options[:product_id])
     end
 
     if options[:process_entity_id]
-      base_query=base_query.where(kanban_process_entities: {process_entity_id: options[:process_entity_id]})
+      q=q.where(kanban_process_entities: {process_entity_id: options[:process_entity_id]})
     end
-    yield(base_query) if block_given?
-    base_query
+	q
+  end
+
+  def query_kanban(options,block)
+    q=base_query_kanban(options).select('count(*) as size,product_id,process_entity_id')
+
+    yield(q) if block_given?
+	if block
+		block.call(q)
+	end
+    q
   end
 
   def process_entity_id
@@ -46,8 +61,8 @@ module KanbanAutoCountable
   def update_rac_proc
    Proc.new { |query|
       query.each_with_index { |item, i|
-        update_recount_auto_copy(item.product_id, item.process_entity_id, item.size)
-      }
+        update_recount_auto_copy(item[:product_id], item[:process_entity_id], item[:size])
+	  }
     }
   end
 
