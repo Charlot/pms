3539dd1702529237e2d0aa7e5388e43485a3cdc8
lzding <472339105@qq.com>
2016-11-24 15:05:23 +0800

    fix

--- pms-server/app/services/printer/p002.rb
+++ pms-server/app/services/printer/p002.rb
@@ -1,10 +1,11 @@
 # Print BAI KANBAN
 module Printer
   class P002<Base
-    HEAD=[:kanban_nr, :part_nr, :wire_nr, :customer_nr, :card_quantity, :safe_quantity, :card_number, :work_time, :send_position,
-          :wire_description, :wire_length, :bundle_number, :strip_length1, :terminal_custom_nr1, :terminal_nr1, :seal_custom_nr1, :seal_nr1,
-          :strip_length2, :terminal_custom_nr2, :terminal_nr2, :seal_custom_nr2, :seal_nr2,
-          :apab_description, :remark, :machine_nr, :kanban_2dcode]
+    # HEAD=[:kanban_nr, :part_nr, :wire_nr, :customer_nr, :card_quantity, :safe_quantity, :card_number, :work_time, :send_position,
+    #       :wire_description, :wire_length, :bundle_number, :strip_length1, :terminal_custom_nr1, :terminal_nr1, :seal_custom_nr1, :seal_nr1,
+    #       :strip_length2, :terminal_custom_nr2, :terminal_nr2, :seal_custom_nr2, :seal_nr2,
+    #       :apab_description, :remark, :machine_nr, :kanban_2dcode]
+    HEAD=[:kanban_nr, :part_nr, :wire_nr, :card_quantity, :safe_quantity, :send_position, :wire_description, :kanban_2dcode]
 
     def generate_data(args=nil)
       @kanban = Kanban.find_by_nr(self.id)
@@ -28,32 +29,32 @@ module Printer
       #应该是固定的，消耗的原材料，填入
       head={
           kanban_nr: @kanban.nr,
-          machine_nr: args[:machine_nr],
+          # machine_nr: args[:machine_nr],
           part_nr: @kanban.product_nr,
           wire_nr: @kanban.wire_nr,
-          customer_nr: @kanban.product_custom_nr,
+          # customer_nr: @kanban.product_custom_nr,
           card_quantity: @kanban.quantity,
           safe_quantity: @kanban.safety_stock.to_i.to_s,
-          card_number: @kanban.copies,
-          work_time: @kanban.task_time,
+          # card_number: @kanban.copies,
+          # work_time: @kanban.task_time,
           send_position: @kanban.des_storage,
           wire_description: wire_desc,
-
+          #
           kanban_2dcode: @kanban.printed_2DCode,
-          wire_length: @process_entity.value_wire_qty_factor,
-          bundle_number: @kanban.bundle,
-          strip_length1: @process_entity.t1_strip_length,
-          terminal_custom_nr1: parts_info[:t1_custom_nr],
-          terminal_nr1: parts_info[:t1_nr],
-          seal_custom_nr1: parts_info[:s1_custom_nr],
-          seal_nr1: parts_info[:s1_nr],
-          strip_length2: @process_entity.t2_strip_length,
-          terminal_custom_nr2: parts_info[:t2_custom_nr],
-          terminal_nr2: parts_info[:t2_nr],
-          seal_custom_nr2: parts_info[:s2_custom_nr],
-          seal_nr2: parts_info[:s2_nr],
-          apab_description: @kanban.remark2,
-          remark: @kanban.remark
+          # wire_length: @process_entity.value_wire_qty_factor,
+          # bundle_number: @kanban.bundle,
+          # strip_length1: @process_entity.t1_strip_length,
+          # terminal_custom_nr1: parts_info[:t1_custom_nr],
+          # terminal_nr1: parts_info[:t1_nr],
+          # seal_custom_nr1: parts_info[:s1_custom_nr],
+          # seal_nr1: parts_info[:s1_nr],
+          # strip_length2: @process_entity.t2_strip_length,
+          # terminal_custom_nr2: parts_info[:t2_custom_nr],
+          # terminal_nr2: parts_info[:t2_nr],
+          # seal_custom_nr2: parts_info[:s2_custom_nr],
+          # seal_nr2: parts_info[:s2_nr],
+          # apab_description: @kanban.remark2,
+          # remark: @kanban.remark
       }
       heads=[]
       HEAD.each do |k|
